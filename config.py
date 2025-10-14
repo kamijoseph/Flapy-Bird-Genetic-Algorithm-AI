6ffae8dcae3a7bac899a60db9bef3b75780f4a58
Kami <josephkiarie561@gmail.com>
2025-10-14 14:44:41 +0300

    the ground

---
+++ config.py
@@ -0,0 +1,6 @@
+
+import pygame
+
+win_height = 900
+win_width = 1200
+window = pygame.display.set_mode((win_width, win_height))
---
+++ main.py
@@ -0,0 +1,25 @@
+
+import pygame
+from sys import exit
+import config
+
+pygame.init()
+clock = pygame.time.Clock()
+
+FPS = 60
+
+def quit_game():
+    for event in pygame.event.get():
+        if event.type == "QUIT":
+            pygame.quit()
+            exit()
+
+def main():
+    while True:
+        quit_game()
+        config.window.fill((0, 0, 0))
+        clock.tick(FPS)
+        pygame.display.flip()
+        
+if __name__ == "__main__":
+    main()
