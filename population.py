87eefa8797cc2f85998b0a41cefa375f21871574
Kami <josephkiarie561@gmail.com>
2025-10-25 21:46:06 +0300

    population extinct function

--- population.py
+++ population.py
@@ -3,11 +3,24 @@ import config
 import player
 
 class Population:
-    def __init__(self):
-        self.player = player.Player()
+    def __init__(self, size):
+        self.players = []
+        self.size = size
+        for i in range(0, self.size):
+            self.players.append(player.Player())
 
     def update_live_players(self):
-        if self.player.alive:
-            self.player.think()
-            self.player.draw(config.window)
-            self.player.update(config.ground)
+        for player in self.players:
+            if player.alive:
+                player.think()
+                player.draw(config.window)
+                player.update(config.ground)
+    
+    # return true if all players are dead
+    def extinct(self):
+        extinct = True
+        for player in self.players:
+            if player.alive:
+                extinct = False
+        
+        return extinct
