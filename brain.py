66094e318825001acc5c4c79e4183601deb1ee29
Kami <josephkiarie561@gmail.com>
2025-10-29 14:49:25 +0300

    clone bain funcntion

--- brain.py
+++ brain.py
@@ -5,31 +5,32 @@ import connection
 import random
 
 class Brain:
-    def __init__(self, inputs):
+    def __init__(self, inputs, clone=False):
         self.connections = []
         self.nodes = []
         self.inputs = inputs
         self.net = []
         self.layers = 2
 
-        # input nodes
-        for i in range(0, self.inputs):
-            self.nodes.append(node.Node(i))
-            self.nodes[i].layer = 0
+        if not clone:
+            # input nodes
+            for i in range(0, self.inputs):
+                self.nodes.append(node.Node(i))
+                self.nodes[i].layer = 0
 
-        # bias node
-        self.nodes.append(node.Node(3))
-        self.nodes[3].layer = 0
-        
-        # output node
-        self.nodes.append(node.Node(4))
-        self.nodes[4].layer = 1
+            # bias node
+            self.nodes.append(node.Node(3))
+            self.nodes[3].layer = 0
+            
+            # output node
+            self.nodes.append(node.Node(4))
+            self.nodes[4].layer = 1
 
-        # connections
-        for i in range(0, 4):
-            self.connections.append(connection.Connection(
-                self.nodes[i], self.nodes[4], random.uniform(-1, 1)
-            ))
+            # connections
+            for i in range(0, 4):
+                self.connections.append(connection.Connection(
+                    self.nodes[i], self.nodes[4], random.uniform(-1, 1)
+                ))
 
     def connect_nodes(self):
         for i in range(0, len(self.nodes)):
@@ -62,3 +63,18 @@ class Brain:
             self.nodes[i].input_value = 0
 
         return output_value
+    
+    def clone(self):
+        clone = Brain(self.inputs, True)
+        for node in self.nodes:
+            clone.nodes.append(node.clone())
+        for connection in self.connections:
+            clone.connections.append(
+                connection.clone(
+                    clone.getNode(connection.from_node.id),
+                    clone.getNode(connection.to_node.id)
+                )
+            )
+        clone.layers = self.layers
+        clone.connect_nodes()
+
